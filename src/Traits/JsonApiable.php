9defd03c0f7b1828742845a14c71024512b50fa3
Steve Harmeyer <harmeyersteve@gmail.com>
2018-06-20 16:36:32 -0700

    updates

--- src/Traits/JsonApiable.php
+++ src/Traits/JsonApiable.php
@@ -11,8 +11,15 @@ trait JsonApiable
             'id' => (string)$this->getKey(),
             'attributes' => $this->attributesToArray(),
         ];
+        // grab includes
         foreach ($this->getRelations() as $relation) {
             $return['included'] = array_merge((array) $return['included'], $relation->toArray());
+        }
+        // format dates properly
+        foreach ($this->getDates() as $dateField) {
+            if (isset($return['attributes'][$dateField])) {
+                $return['attributes'][$dateField] = $this->$dateField->toIso8601String();
+            }
         }
         return $return;
     }
